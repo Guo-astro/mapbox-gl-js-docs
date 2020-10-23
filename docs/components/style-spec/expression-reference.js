3bfb2e56190c8bb900effeb28ddb2b4b38ebaeb1
Katy DeCorah <decorah@mapbox.com>
2020-10-23 13:29:26 -0400

    Add related section to expressions; add example title

--- docs/components/style-spec/expression-reference.js
+++ docs/components/style-spec/expression-reference.js
@@ -6,12 +6,29 @@ import md from '../md';
 import { highlightJavascript } from '../prism_highlight';
 import { renderSignature } from './render-signature';
 import Property from './property.js';
+import related from '../../data/expressions-related.json';
 
 export default class ExpressionReference extends React.Component {
     render() {
         const group = groupedExpressions.filter(
             (g) => g.name === this.props.group
         )[0];
+
+        const Related = ({ links }) => {
+            if (!links || !links.length === 0) return;
+            return (
+                <div className="">
+                    <div className="txt-bold mb6">Related</div>
+                    <ul className="mb18">
+                        {links.map((link) => (
+                            <li key={link.title}>
+                                <a href={link.href}>{link.title}</a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            );
+        };
         return (
             <React.Fragment>
                 {group.expressions.map(({ name, doc, type, sdkSupport }) => (
@@ -24,8 +41,9 @@ export default class ExpressionReference extends React.Component {
                             {name}
                         </Property>
 
-                        {doc && <div>{md(doc)}</div>}
+                        {doc && <div className="mb12">{md(doc)}</div>}
 
+                        <div className="txt-bold mb6">Example</div>
                         {type.map((overload, i) => (
                             <div key={i}>
                                 {highlightJavascript(
@@ -33,6 +51,8 @@ export default class ExpressionReference extends React.Component {
                                 )}
                             </div>
                         ))}
+                        {related[name] && <Related links={related[name]} />}
+
                         {sdkSupport && <SDKSupportTable {...sdkSupport} />}
                     </React.Fragment>
                 ))}
